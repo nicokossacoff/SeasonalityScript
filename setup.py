1d01212a6fafef2201d4182f4cde46d1ad561ab3
Nicolás Kossacoff <nicokossacoff@gmail.com>
2024-03-03 16:50:31 -0300

    Included a setup file (#2)

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(name= 'SeasonalityScript',
+      version= '1.0.0',
+      description= 'Bank holidays and seasonality variables creation',
+      author= 'Nicolás Kossacoff',
+      author_email= 'nicokossacoff@gmail.com',
+      license= 'Apache',
+      packages= setuptools.find_packages(),
+      classifiers= ["Programming Language :: Python :: 3", "License :: OSI Approved :: Apache License"],
+      zip_safe= False)
