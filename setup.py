67b267ace9d536a5932e1b418a730ac7af2d3ff2
Nicolas Kossacoff <nicokossacoff@gmail.com>
2024-03-10 23:15:08 -0300

    Updated setup file with required packages to install

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ import setuptools
 from setuptools import setup
 
 setup(name= 'SeasonalityScript',
+      packages= ["SeasonalityScript"],
       version= '1.0.0',
       description= 'Bank holidays and seasonality variables creation',
       author= 'Nicolás Kossacoff',
       author_email= 'nicokossacoff@gmail.com',
+      install_requires= ["pandas>=2.0.0",
+                         "numpy>=1.26.4",
+                         "requests>=2.31.0"],
       license= 'Apache',
       packages= setuptools.find_packages(),
       classifiers= ["Programming Language :: Python :: 3", "License :: OSI Approved :: Apache License"],
